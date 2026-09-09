6371ebdb65dc46effc7fcfabf7faff15985cc99e
hashem a.alshami <hashalshamii@gmail.com>
2026-09-09 22:02:33 +0300

    style: sort model exports

--- src/mediadl/models/__init__.py
+++ src/mediadl/models/__init__.py
@@ -1,3 +1,9 @@
-from .models import DownloadResult, Format, MediaInfo, ProgressEvent, PlaylistResult
+from .models import DownloadResult, Format, MediaInfo, PlaylistResult, ProgressEvent
 
-__all__ = ["DownloadResult", "Format", "MediaInfo", "ProgressEvent", "PlaylistResult"]
+__all__ = [
+    "DownloadResult",
+    "Format",
+    "MediaInfo",
+    "PlaylistResult",
+    "ProgressEvent",
+]
